16c19b14aefb5ffe4029df8459562e9f08bb94aa
Juan B Cabral <jbc.develop@gmail.com>
2017-10-21 22:39:47 -0300

    better_assertions

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,6 +5,9 @@ import sys
 import argparse
 import inspect
 import io
+import unittest
+
+import sh
 
 import numpy as np
 
@@ -20,12 +23,18 @@ class SKoTreeTestCase(object):
         self.has_session = has_session
         self.methods = []
         self.is_remote = skotree.is_url(path)
+        self.check = unittest.TestCase('__init__')
         for mname in dir(self):
             method = getattr(self, mname)
             if mname.startswith("test_") and inspect.ismethod(method):
                 self.methods.append(method)
 
+    def __getattr__(self, n):
+        return getattr(self.check, n)
+
     def setUp(self):
+        if self.is_remote:
+            self.proc = sh.otree.runserver("localhost:6859", _bg=True)
         self.otree = skotree.oTree(self.path)
 
     def tearDown(self):
@@ -43,83 +52,68 @@ class SKoTreeTestCase(object):
 
     def test_lsapps(self):
         apps = self.otree.lsapps()
-        assert apps == ["matching_pennies"]
+        self.assertEquals(apps, ["matching_pennies"])
 
     def test_lssessions(self):
         apps = self.otree.lssessions()
-        assert apps == ["matching_pennies"]
+        self.assertEquals(apps, ["matching_pennies"])
 
     def test_session_config(self):
         self.otree.session_config("matching_pennies")
-        try:
+        with self.assertRaises():
             self.otree.session_config("foo")
-        except:
-            pass
-        else:
-            raise AssertionError()
 
     def test_all_data(self):
         all_data = self.otree.all_data()
         must_be_empty = not self.has_session
-        assert all_data.empty == must_be_empty
-        assert all_data.columns.size == (49 if self.has_session else 0)
+        self.assertEquals(all_data.empty, must_be_empty)
+        self.assertEquals(
+            all_data.columns.size, (49 if self.has_session else 0))
 
     def test_time_spent(self):
         tspent = self.otree.time_spent()
-        assert tspent.empty == True
-        assert len(tspent.columns) == 10
+        self.assertTrue(tspent.empty)
+        self.assertEquals(len(tspent.columns), 10)
 
     def test_app_data_fail(self):
-        try:
+        with self.assertRaises(Exception):
             self.otree.app_data("foo")
-        except:
-            pass
-        else:
-            raise AssertionError()
 
     def test_app_data(self):
         data = self.otree.app_data("matching_pennies")
         must_be_empty = not self.has_session
-        assert data.empty == must_be_empty
-        assert data.columns.size == 28
+        self.assertEquals(data.empty, must_be_empty)
+        self.assertEquals(data.columns.size, 28)
 
     def test_app_doc_fail(self):
-        try:
+        with self.assertRaises(Exception):
             self.otree.app_doc("foo")
-        except:
-            pass
-        else:
-            raise AssertionError()
 
     def test_app_doc(self):
         doc = self.otree.app_doc("matching_pennies")
-        assert isinstance(doc, str)
+        self.assertIsInstance(doc, str)
 
     def test_bot_data(self):
         store = self.otree.bot_data("matching_pennies", 2)
-        assert isinstance(store.matching_pennies, pd.DataFrame)
-        assert isinstance(store["matching_pennies"], pd.DataFrame)
-        assert "matching_pennies" in store
+        self.assertIsInstance(store.matching_pennies, pd.DataFrame)
+        self.assertIsInstance(store["matching_pennies"], pd.DataFrame)
+        self.assertIn("matching_pennies", store)
 
         df = store.matching_pennies
-        assert np.unique(df["participant.code"]).size == 2
+        self.assertEquals(np.unique(df["participant.code"]).size, 2)
 
         df = self.otree.bot_data("matching_pennies", 4).matching_pennies
-        assert np.unique(df["participant.code"]).size == 4
+        self.assertEquals(np.unique(df["participant.code"]).size, 4)
 
     def test_bot_data_fail(self):
-        try:
+        with self.assertRaises(Exception):
             self.otree.bot_data("matching_pennies", 3)
-        except RuntimeError:
-            pass
-        else:
-            raise AssertionError()
 
     def test_csv_store(self):
         store = skotree.CSVStore({"foo": io.StringIO()})
-        assert "foo" in store
-        assert "<CSVStore({foo})>" == repr(store)
-        assert store.foo.empty
+        self.assertIn("foo", store)
+        self.assertEquals("<CSVStore({foo})>" == repr(store))
+        self.assertTrue(store.foo.empty)
 
 
 if __name__ == "__main__":
